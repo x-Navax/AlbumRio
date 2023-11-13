aa7967f376965616befe79259c5c4030ebc20cf8
Tobias <tobias_crugnale@hotmail.com>
2023-11-13 17:49:09 -0300

    fotos faltantes

--- main.js
+++ main.js
@@ -11,35 +11,51 @@ function loadDriveAPI() {
 }
 
 function listFiles() {
-  gapi.client.drive.files.list({
-      q: "'1kebSm7_K0eCXfx5by28ZOVHMMLEGxvn2' in parents",
-      fields: "files(id, name, webContentLink, webViewLink)" // Agregamos webViewLink para el enlace de vista en Drive
-  }).then(function(response) {
-      var files = response.result.files;
-      var imageContainer = document.getElementById('imageContainer');
-      if (files && files.length > 0) {
-        files.forEach(function(file) {
-            var img = document.createElement('img');
-            img.src = file.webContentLink;
-            img.alt = file.name;
+  var nextPageToken = null;
 
-            var downloadButton = document.createElement('a');
-            downloadButton.textContent = 'Descargar';
-            downloadButton.href = file.webContentLink; // Enlace directo al archivo
-            downloadButton.download = file.name; // Nombre de archivo para la descarga
-            downloadButton.setAttribute('target', '_blank'); // Abre el enlace en una nueva pestaña
+  function retrieveFiles() {
+      gapi.client.drive.files.list({
+          q: "'1kebSm7_K0eCXfx5by28ZOVHMMLEGxvn2' in parents",
+          fields: "nextPageToken, files(id, name, webContentLink, webViewLink)",
+          pageSize: 1000, // Define el tamaño máximo de página (máximo permitido por la API)
+          pageToken: nextPageToken // Utiliza el token de la página siguiente si hay más resultados
+      }).then(function(response) {
+          var files = response.result.files;
+          nextPageToken = response.result.nextPageToken; // Guarda el token de la página siguiente para la siguiente llamada
 
-            var imageWrapper = document.createElement('div');
-            imageWrapper.appendChild(img);
-            imageWrapper.appendChild(downloadButton);
+          var imageContainer = document.getElementById('imageContainer');
 
-            imageContainer.appendChild(imageWrapper);
-        });
-    } else {
-        console.log('No se encontraron archivos.');
-      }
-  });
+          if (files && files.length > 0) {
+              files.forEach(function(file) {
+                  var img = document.createElement('img');
+                  img.src = file.webContentLink;
+                  img.alt = file.name;
+
+                  var downloadButton = document.createElement('a');
+                  downloadButton.textContent = 'Descargar';
+                  downloadButton.href = file.webContentLink; // Enlace directo al archivo
+                  downloadButton.download = file.name; // Nombre de archivo para la descarga
+                  downloadButton.setAttribute('target', '_blank'); // Abre el enlace en una nueva pestaña
+
+                  var imageWrapper = document.createElement('div');
+                  imageWrapper.appendChild(img);
+                  imageWrapper.appendChild(downloadButton);
+
+                  imageContainer.appendChild(imageWrapper);
+              });
+
+              if (nextPageToken) {
+                  retrieveFiles(); // Si hay más archivos, obtener la siguiente página
+              }
+          } else {
+              console.log('No se encontraron archivos.');
+          }
+      });
+  }
+
+  retrieveFiles(); // Llamada inicial para comenzar a obtener los archivos
 }
+
 
 function onGoogleScriptLoad() {
   gapi.load('client', start);
