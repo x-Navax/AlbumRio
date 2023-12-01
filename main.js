36edeb0a1bc276ee8a526025e75fc4237009dd6a
x-Navax <126367945+x-Navax@users.noreply.github.com>
2023-12-01 19:58:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function listFiles() {
 
   function retrieveFiles() {
       gapi.client.drive.files.list({
-          q: "'1uvbTuYe91MkjmxKdSylnaF3-qtVCKbu-' in parents",
+          q: "'14ALugkifVa9_28U-p78NicBUNPYvTIhq' in parents",
           fields: "nextPageToken, files(id, name, webContentLink, webViewLink)",
           pageSize: 1000, // Define el tamaño máximo de página (máximo permitido por la API)
           pageToken: nextPageToken // Utiliza el token de la página siguiente si hay más resultados
